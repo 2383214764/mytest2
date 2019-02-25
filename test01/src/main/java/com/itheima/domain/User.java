83d300fbdb352b175f6a8cbe397c96bee3aadd57
qiankang <2383214764@qq.com>
2019-02-25 08:46:36 +0800

    hhah

---
+++ test01/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,51 @@
+package com.itheima.domain;
+
+public class User {
+    private Integer uid;
+    private String username;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", rolename='" + rolename + '\'' +
+                '}';
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRolename() {
+        return rolename;
+    }
+
+    public void setRolename(String rolename) {
+        this.rolename = rolename;
+    }
+
+    private String rolename;
+}
